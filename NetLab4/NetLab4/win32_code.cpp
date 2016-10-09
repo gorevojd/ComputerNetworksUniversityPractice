4006b6f4ee9a80b675e974a90c0ba81da3ddbab7
Gorevoy Dima <gorevojd27@gmail.com>
2016-10-09 21:23:23 +0300

    Did 4 lab

--- NetLab4/NetLab4/win32_code.cpp
+++ NetLab4/NetLab4/win32_code.cpp
@@ -2,61 +2,137 @@
 #include <WinSock2.h>
 #include <Windows.h>
 
-
 using namespace std;
 
 void CheckNetError();
-bool GetRequestFromClient(char* name, short port, sockaddr* from, int* flen);
+
+void PrintInfo(sockaddr_in* info){
+	printf("IP: %s. PORT: %d.\n", inet_ntoa(info->sin_addr), info->sin_port);
+}
+
+bool GetRequestFromClient(
+	SOCKET ServerSocket,
+	char* ServerName,
+	int ServerNameLen,
+	sockaddr* Clnt,
+	int* ClntLen)
+{
+	bool Result = false;
+
+	int receivedByteCount = recvfrom(
+		ServerSocket,
+		ServerName,
+		ServerNameLen,
+		0,
+		Clnt,
+		ClntLen);
+
+	if (receivedByteCount > 0){
+		Result = true;
+	}
+	else{
+		CheckNetError();
+	}
+
+	return(Result);
+}
+
+bool SendAnswerToClient(
+	SOCKET ServerSocket,
+	char* ServerName,
+	int ServerNameLen,
+	sockaddr* TargetClient,
+	int TargetClientLen)
+{
+	bool Result = false;
+
+	int SentBytesCount = sendto(
+		ServerSocket,
+		ServerName,
+		ServerNameLen,
+		0,
+		TargetClient,
+		TargetClientLen);
+
+	
+
+	return(Result);
+}
+
 
 //SERVER
 int main(int argc, char** argv){
-
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 0), &wsaData);
 	CheckNetError();
 
-	SOCKET serverSock = socket(AF_INET, SOCK_DGRAM, NULL);
-	CheckNetError();
+	char ServerName[] = "Hello";
 
-	int optVal = 1;
-	setsockopt(serverSock, SOL_SOCKET, SO_BROADCAST, (char*)&optVal, sizeof(int));
+	SOCKET ServerSocket = socket(AF_INET, SOCK_DGRAM, NULL);
 	CheckNetError();
 
 	SOCKADDR_IN serv;
 	serv.sin_family = AF_INET;
 	serv.sin_port = htons(2000);
 	serv.sin_addr.s_addr = INADDR_ANY;
+	if (bind(ServerSocket, (LPSOCKADDR)&serv, sizeof(serv)) >= 0){
 
-	bind(serverSock, (LPSOCKADDR)&serv, sizeof(serv));
-	CheckNetError();
+		bool Receiving = true;
+		while (Receiving == true){
+#if 0
+			SOCKADDR_IN serv;
+			serv.sin_family = AF_INET;
+			serv.sin_port = htons(2000);
+			serv.sin_addr.s_addr = INADDR_ANY;
 
-	SOCKADDR_IN clnt;
-	memset(&clnt, 0, sizeof(clnt));
-	int clientLen = sizeof(clnt);
-	char inputBuffer[50];
-	int receivedByteCount = 0;
-	receivedByteCount = recvfrom(
-		serverSock,
-		inputBuffer,
-		sizeof(inputBuffer),
-		0,
-		(sockaddr*)&clnt,
-		&clientLen);
+			bind(ServerSocket, (LPSOCKADDR)&serv, sizeof(serv));
+			CheckNetError();
 
-	cout << inputBuffer << endl;
+			SOCKADDR_IN clnt;
+			memset(&clnt, 0, sizeof(clnt));
+			int clientLen = sizeof(clnt);
 
-	/*char outputBuffer[50] = "Hello from Server";
-	int sentBytesCount = 0;
+			char inputBuffer[50];
+			int receivedByteCount = 0;
+			receivedByteCount = recvfrom(
+				ServerSocket,
+				inputBuffer,
+				sizeof(inputBuffer),
+				0,
+				(sockaddr*)&clnt,
+				&clientLen);
 
-	sentBytesCount = sendto(
-		serverSock,
-		outputBuffer,
-		strlen(outputBuffer) + 1,
-		0,
-		(sockaddr*)&clnt,
-		clientLen);*/
+			PrintInfo(&clnt);
 
-	closesocket(serverSock);
+			cout << inputBuffer << endl;
+#else
+
+
+			char RequestStr[50];
+			SOCKADDR_IN Clnt = {};
+			int ClientLen = sizeof(Clnt);
+
+			char Query[50];
+			GetRequestFromClient(ServerSocket, Query, sizeof(Query), (sockaddr*)&Clnt, &ClientLen);
+			int StringsAreEqual = strcmp(ServerName, Query);
+			if (StringsAreEqual == 0){
+				SendAnswerToClient(
+					ServerSocket,
+					ServerName,
+					strlen(ServerName) + 1,
+					(sockaddr*)&Clnt,
+					ClientLen);
+
+				PrintInfo(&Clnt);
+			}
+		}
+	}
+#endif
+	else{
+		CheckNetError();
+	}
+
+	closesocket(ServerSocket);
 	CheckNetError();
 
 	WSACleanup();
@@ -66,195 +142,160 @@ int main(int argc, char** argv){
 	return 0;
 }
 
-bool GetRequestFromClient(char* name, short port, sockaddr* from, int* flen){
-	SOCKET serverSock = socket(AF_INET, SOCK_DGRAM, NULL);
-	CheckNetError();
-
-	int optVal = 1;
-	setsockopt(serverSock, SOL_SOCKET, SO_BROADCAST, (char*)&optVal, sizeof(int));
-	CheckNetError();
-
-	SOCKADDR_IN serv;
-	serv.sin_family = AF_INET;
-	serv.sin_port = htons(port);
-	serv.sin_addr.s_addr = INADDR_ANY;
-
-	bind(serverSock, (LPSOCKADDR)&serv, sizeof(serv));
-	CheckNetError();
-
-	char inputBuffer[50];
-	int receivedByteCount = 0;
-	receivedByteCount = recvfrom(
-		serverSock,
-		inputBuffer,
-		sizeof(inputBuffer),
-		0,
-		(sockaddr*)&from,
-		flen);
-
-	return true;
-}
-
-bool SendAnswerToClient(
-	char* name,
-	sockaddr* targetClient,
-	int* targetClientLen)
-{
-	
-}
 
 void CheckNetError(){
 	int errCode = WSAGetLastError();
 	switch (errCode){
-	case (WSAEINTR) : {
-		OutputDebugStringA("WSAEINTR Работа функции прервана");
-	}break;
-	case(WSAEACCES) : {
-		OutputDebugStringA("WSAEACCES Разрешение отвергнуто");
-	}break;
-	case(WSAEFAULT) : {
-		OutputDebugStringA("WSAEFAULT Ошибочный адрес ");
-	}break;
-	case(WSAEINVAL) : {
-		OutputDebugStringA("WSAEINVAL Ошибка в аргументе");
-	}break;
-	case(WSAEMFILE) : {
-		OutputDebugStringA("WSAEMFILE Слишком много файлов открыто");
-	}break;
-	case(WSAEWOULDBLOCK) : {
-		OutputDebugStringA("WSAEWOULDBLOCK Ресурс временно недоступен");
-	}break;
-	case(WSAEINPROGRESS) : {
-		OutputDebugStringA("WSAEINPROGRESS Операция в процессе развития");
-	}break;
-	case(WSAEALREADY) : {
-		OutputDebugStringA("WSAEALREADY Операция уже выполняется");
-	}break;
-	case(WSAENOTSOCK) : {
-		OutputDebugStringA("WSAENOTSOCK Сокет задан неправильно");
-	}break;
-	case(WSAEDESTADDRREQ) : {
-		OutputDebugStringA("WSAEDESTADDRREQ Требуется адрес расположения ");
-	}break;
-	case(WSAEMSGSIZE) : {
-		OutputDebugStringA("WSAEMSGSIZE Сообщение слишком длинное");
-	}break;
-	case(WSAEPROTOTYPE) : {
-		OutputDebugStringA("WSAEPROTOTYPE Неправильный тип протокола для сокета");
-	}break;
-	case(WSAENOPROTOOPT) : {
-		OutputDebugStringA("WSAENOPROTOOPT Ошибка в опции протокола");
-	}break;
-	case(WSAEPROTONOSUPPORT) : {
-		OutputDebugStringA("WSAEPROTONOSUPPORT Протокол не поддерживается ");
-	}break;
-	case(WSAESOCKTNOSUPPORT) : {
-		OutputDebugStringA("WSAESOCKTNOSUPPORT Тип сокета не поддерживается ");
-	}break;
-	case(WSAEOPNOTSUPP) : {
-		OutputDebugStringA("WSAEOPNOTSUPP Операция не поддерживается ");
-	}break;
-	case(WSAEPFNOSUPPORT) : {
-		OutputDebugStringA("WSAEPFNOSUPPORT Тип протоколов не поддерживается");
-	}break;
-	case(WSAEAFNOSUPPORT) : {
-		OutputDebugStringA("WSAEAFNOSUPPORT Тип адресов не поддерживается протоколом");
-	}break;
-	case(WSAEADDRINUSE) : {
-		OutputDebugStringA("WSAEADDRINUSE Адрес уже используется");
-	}break;
-	case(WSAEADDRNOTAVAIL) : {
-		OutputDebugStringA("WSAEADDRNOTAVAIL Запрошенный адрес не может быть использован");
-	}break;
-	case(WSAENETDOWN) : {
-		OutputDebugStringA("WSAENETDOWN Сеть отключена");
-	}break;
-	case(WSAENETUNREACH) : {
-		OutputDebugStringA("WSAENETUNREACH Сеть не достижима ");
-	}break;
-	case(WSAENETRESET) : {
-		OutputDebugStringA("WSAENETRESET Сеть разорвала соединение");
-	}break;
-	case(WSAECONNABORTED) : {
-		OutputDebugStringA("WSAECONNABORTED Программный отказ связи ");
-	}break;
-	case(WSAECONNRESET) : {
-		OutputDebugStringA("WSAECONNRESET Связь восстановлена ");
-	}break;
-	case(WSAENOBUFS) : {
-		OutputDebugStringA("WSAENOBUFS Не хватает памяти для буферов");
-	}break;
-	case(WSAEISCONN) : {
-		OutputDebugStringA("WSAEISCONN Сокет уже подключен");
-	}break;
-	case(WSAENOTCONN) : {
-		OutputDebugStringA("WSAENOTCONN Сокет не подключен");
-	}break;
-	case(WSAESHUTDOWN) : {
-		OutputDebugStringA("WSAESHUTDOWN Нельзя выполнить send: сокет завершил работу");
-	}break;
-	case(WSAETIMEDOUT) : {
-		OutputDebugStringA("WSAETIMEDOUT Закончился отведенный интервал  времени");
-	}break;
-	case(WSAECONNREFUSED) : {
-		OutputDebugStringA("WSAECONNREFUSED Соединение отклоненo");
-	}break;
-	case(WSAEHOSTDOWN) : {
-		OutputDebugStringA("WSAEHOSTDOWN Хост в неработоспособном состоянии");
-	}break;
-	case(WSAEHOSTUNREACH) : {
-		OutputDebugStringA("WSAEHOSTUNREACH Нет маршрута для хоста");
-	}break;
-	case(WSAEPROCLIM) : {
-		OutputDebugStringA("WSAEPROCLIM Слишком много процессов ");
-	}break;
-	case(WSASYSNOTREADY) : {
-		OutputDebugStringA("WSASYSNOTREADY Сеть не доступна ");
-	}break;
-	case(WSAVERNOTSUPPORTED) : {
-		OutputDebugStringA("WSAVERNOTSUPPORTED Данная версия недоступна ");
-	}break;
-	case(WSANOTINITIALISED) : {
-		OutputDebugStringA("WSANOTINITIALISED Не выполнена инициализация WS2_32.DLL");
-	}break;
-	case(WSAEDISCON) : {
-		OutputDebugStringA("WSAEDISCON Выполняется отключение");
-	}break;
-	case(WSATYPE_NOT_FOUND) : {
-		OutputDebugStringA("WSATYPE_NOT_FOUND Класс не найден");
-	}break;
-	case(WSAHOST_NOT_FOUND) : {
-		OutputDebugStringA("WSAHOST_NOT_FOUND Хост не найден ");
-	}break;
-	case(WSATRY_AGAIN) : {
-		OutputDebugStringA("WSATRY_AGAIN Неавторизированный хост не найден ");
-	}break;
-	case(WSANO_RECOVERY) : {
-		OutputDebugStringA("WSANO_RECOVERY Неопределенная  ошибка");
-	}break;
-	case(WSANO_DATA) : {
-		OutputDebugStringA("WSANO_DATA Нет записи запрошенного типа");
-	}break;
-	case(WSA_INVALID_HANDLE) : {
-		OutputDebugStringA("WSA_INVALID_HANDLE Указанный дескриптор события  с ошибкой");
-	}break;
-	case(WSA_INVALID_PARAMETER) : {
-		OutputDebugStringA("WSA_INVALID_PARAMETER Один или более параметров с ошибкой");
-	}break;
-	case(WSA_IO_INCOMPLETE) : {
-		OutputDebugStringA("WSA_IO_INCOMPLETE Объект ввода-вывода не в сигнальном состоянии");
-	}break;
-	case(WSA_IO_PENDING) : {
-		OutputDebugStringA("WSA_IO_PENDING Операция завершится позже");
-	}break;
-	case(WSA_NOT_ENOUGH_MEMORY) : {
-		OutputDebugStringA("WSA_NOT_ENOUGH_MEMORY Не достаточно памяти");
-	}break;
-	case(WSA_OPERATION_ABORTED) : {
-		OutputDebugStringA("WSA_OPERATION_ABORTED Операция отвергнута");
-	}break;
-	case(0) : {
-		OutputDebugStringA("Everything is seems to be OK\n");
-	}break;
+		case (WSAEINTR) : {
+			OutputDebugStringA("WSAEINTR Работа функции прервана");
+		}break;
+		case(WSAEACCES) : {
+			OutputDebugStringA("WSAEACCES Разрешение отвергнуто");
+		}break;
+		case(WSAEFAULT) : {
+			OutputDebugStringA("WSAEFAULT Ошибочный адрес ");
+		}break;
+		case(WSAEINVAL) : {
+			OutputDebugStringA("WSAEINVAL Ошибка в аргументе");
+		}break;
+		case(WSAEMFILE) : {
+			OutputDebugStringA("WSAEMFILE Слишком много файлов открыто");
+		}break;
+		case(WSAEWOULDBLOCK) : {
+			OutputDebugStringA("WSAEWOULDBLOCK Ресурс временно недоступен");
+		}break;
+		case(WSAEINPROGRESS) : {
+			OutputDebugStringA("WSAEINPROGRESS Операция в процессе развития");
+		}break;
+		case(WSAEALREADY) : {
+			OutputDebugStringA("WSAEALREADY Операция уже выполняется");
+		}break;
+		case(WSAENOTSOCK) : {
+			OutputDebugStringA("WSAENOTSOCK Сокет задан неправильно");
+		}break;
+		case(WSAEDESTADDRREQ) : {
+			OutputDebugStringA("WSAEDESTADDRREQ Требуется адрес расположения ");
+		}break;
+		case(WSAEMSGSIZE) : {
+			OutputDebugStringA("WSAEMSGSIZE Сообщение слишком длинное");
+		}break;
+		case(WSAEPROTOTYPE) : {
+			OutputDebugStringA("WSAEPROTOTYPE Неправильный тип протокола для сокета");
+		}break;
+		case(WSAENOPROTOOPT) : {
+			OutputDebugStringA("WSAENOPROTOOPT Ошибка в опции протокола");
+		}break;
+		case(WSAEPROTONOSUPPORT) : {
+			OutputDebugStringA("WSAEPROTONOSUPPORT Протокол не поддерживается ");
+		}break;
+		case(WSAESOCKTNOSUPPORT) : {
+			OutputDebugStringA("WSAESOCKTNOSUPPORT Тип сокета не поддерживается ");
+		}break;
+		case(WSAEOPNOTSUPP) : {
+			OutputDebugStringA("WSAEOPNOTSUPP Операция не поддерживается ");
+		}break;
+		case(WSAEPFNOSUPPORT) : {
+			OutputDebugStringA("WSAEPFNOSUPPORT Тип протоколов не поддерживается");
+		}break;
+		case(WSAEAFNOSUPPORT) : {
+			OutputDebugStringA("WSAEAFNOSUPPORT Тип адресов не поддерживается протоколом");
+		}break;
+		case(WSAEADDRINUSE) : {
+			OutputDebugStringA("WSAEADDRINUSE Адрес уже используется");
+		}break;
+		case(WSAEADDRNOTAVAIL) : {
+			OutputDebugStringA("WSAEADDRNOTAVAIL Запрошенный адрес не может быть использован");
+		}break;
+		case(WSAENETDOWN) : {
+			OutputDebugStringA("WSAENETDOWN Сеть отключена");
+		}break;
+		case(WSAENETUNREACH) : {
+			OutputDebugStringA("WSAENETUNREACH Сеть не достижима ");
+		}break;
+		case(WSAENETRESET) : {
+			OutputDebugStringA("WSAENETRESET Сеть разорвала соединение");
+		}break;
+		case(WSAECONNABORTED) : {
+			OutputDebugStringA("WSAECONNABORTED Программный отказ связи ");
+		}break;
+		case(WSAECONNRESET) : {
+			OutputDebugStringA("WSAECONNRESET Связь восстановлена ");
+		}break;
+		case(WSAENOBUFS) : {
+			OutputDebugStringA("WSAENOBUFS Не хватает памяти для буферов");
+		}break;
+		case(WSAEISCONN) : {
+			OutputDebugStringA("WSAEISCONN Сокет уже подключен");
+		}break;
+		case(WSAENOTCONN) : {
+			OutputDebugStringA("WSAENOTCONN Сокет не подключен");
+		}break;
+		case(WSAESHUTDOWN) : {
+			OutputDebugStringA("WSAESHUTDOWN Нельзя выполнить send: сокет завершил работу");
+		}break;
+		case(WSAETIMEDOUT) : {
+			OutputDebugStringA("WSAETIMEDOUT Закончился отведенный интервал  времени");
+		}break;
+		case(WSAECONNREFUSED) : {
+			OutputDebugStringA("WSAECONNREFUSED Соединение отклоненo");
+		}break;
+		case(WSAEHOSTDOWN) : {
+			OutputDebugStringA("WSAEHOSTDOWN Хост в неработоспособном состоянии");
+		}break;
+		case(WSAEHOSTUNREACH) : {
+			OutputDebugStringA("WSAEHOSTUNREACH Нет маршрута для хоста");
+		}break;
+		case(WSAEPROCLIM) : {
+			OutputDebugStringA("WSAEPROCLIM Слишком много процессов ");
+		}break;
+		case(WSASYSNOTREADY) : {
+			OutputDebugStringA("WSASYSNOTREADY Сеть не доступна ");
+		}break;
+		case(WSAVERNOTSUPPORTED) : {
+			OutputDebugStringA("WSAVERNOTSUPPORTED Данная версия недоступна ");
+		}break;
+		case(WSANOTINITIALISED) : {
+			OutputDebugStringA("WSANOTINITIALISED Не выполнена инициализация WS2_32.DLL");
+		}break;
+		case(WSAEDISCON) : {
+			OutputDebugStringA("WSAEDISCON Выполняется отключение");
+		}break;
+		case(WSATYPE_NOT_FOUND) : {
+			OutputDebugStringA("WSATYPE_NOT_FOUND Класс не найден");
+		}break;
+		case(WSAHOST_NOT_FOUND) : {
+			OutputDebugStringA("WSAHOST_NOT_FOUND Хост не найден ");
+		}break;
+		case(WSATRY_AGAIN) : {
+			OutputDebugStringA("WSATRY_AGAIN Неавторизированный хост не найден ");
+		}break;
+		case(WSANO_RECOVERY) : {
+			OutputDebugStringA("WSANO_RECOVERY Неопределенная  ошибка");
+		}break;
+		case(WSANO_DATA) : {
+			OutputDebugStringA("WSANO_DATA Нет записи запрошенного типа");
+		}break;
+		case(WSA_INVALID_HANDLE) : {
+			OutputDebugStringA("WSA_INVALID_HANDLE Указанный дескриптор события  с ошибкой");
+		}break;
+		case(WSA_INVALID_PARAMETER) : {
+			OutputDebugStringA("WSA_INVALID_PARAMETER Один или более параметров с ошибкой");
+		}break;
+		case(WSA_IO_INCOMPLETE) : {
+			OutputDebugStringA("WSA_IO_INCOMPLETE Объект ввода-вывода не в сигнальном состоянии");
+		}break;
+		case(WSA_IO_PENDING) : {
+			OutputDebugStringA("WSA_IO_PENDING Операция завершится позже");
+		}break;
+		case(WSA_NOT_ENOUGH_MEMORY) : {
+			OutputDebugStringA("WSA_NOT_ENOUGH_MEMORY Не достаточно памяти");
+		}break;
+		case(WSA_OPERATION_ABORTED) : {
+			OutputDebugStringA("WSA_OPERATION_ABORTED Операция отвергнута");
+		}break;
+		case(0) : {
+			OutputDebugStringA("Everything is seems to be OK\n");
+		}break;
 	}
+	OutputDebugStringA("\n");
 }
--- NetLab4/NetLab4Server/win32_code.cpp
+++ NetLab4/NetLab4Server/win32_code.cpp
@@ -4,8 +4,18 @@
 
 void CheckNetError();
 
+
+void PrintInfo(sockaddr_in* info){
+	printf("SERVER IP: %s. SERVER PORT: %d.\n", inet_ntoa(info->sin_addr), info->sin_port);
+}
+
+
 //Client
 int main(int argc, char** argv){
+
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 0), &wsaData);
+	CheckNetError();
 
 	SOCKET clientSock = socket(AF_INET, SOCK_DGRAM, 0);
 	CheckNetError();
@@ -18,8 +28,8 @@ int main(int argc, char** argv){
 	all.sin_family = AF_INET;
 	all.sin_port = htons(2000);
 	all.sin_addr.s_addr = INADDR_BROADCAST;
-	
-	char outputBuffer[] = "Hello everybody!!!";
+
+	char outputBuffer[] = "Hello from Client";
 
 	int sendLen = sendto(
 		clientSock,
@@ -27,6 +37,20 @@ int main(int argc, char** argv){
 		sizeof(outputBuffer),
 		0,
 		(sockaddr*)&all, sizeof(all));
+	PrintInfo(&all);
+	CheckNetError();
+
+	int allLen = sizeof(all);
+	char inputBuffer[50];
+	int receivedBytesCount = recvfrom(
+		clientSock,
+		inputBuffer,
+		sizeof(inputBuffer),
+		0, (sockaddr*)&all, &allLen);
+
+	printf("%s\n", inputBuffer);
+
+	WSACleanup();
 	CheckNetError();
 
 	system("pause");
@@ -36,155 +60,156 @@ int main(int argc, char** argv){
 void CheckNetError(){
 	int errCode = WSAGetLastError();
 	switch (errCode){
-	case (WSAEINTR) : {
-		OutputDebugStringA("WSAEINTR Работа функции прервана");
-	}break;
-	case(WSAEACCES) : {
-		OutputDebugStringA("WSAEACCES Разрешение отвергнуто");
-	}break;
-	case(WSAEFAULT) : {
-		OutputDebugStringA("WSAEFAULT Ошибочный адрес ");
-	}break;
-	case(WSAEINVAL) : {
-		OutputDebugStringA("WSAEINVAL Ошибка в аргументе");
-	}break;
-	case(WSAEMFILE) : {
-		OutputDebugStringA("WSAEMFILE Слишком много файлов открыто");
-	}break;
-	case(WSAEWOULDBLOCK) : {
-		OutputDebugStringA("WSAEWOULDBLOCK Ресурс временно недоступен");
-	}break;
-	case(WSAEINPROGRESS) : {
-		OutputDebugStringA("WSAEINPROGRESS Операция в процессе развития");
-	}break;
-	case(WSAEALREADY) : {
-		OutputDebugStringA("WSAEALREADY Операция уже выполняется");
-	}break;
-	case(WSAENOTSOCK) : {
-		OutputDebugStringA("WSAENOTSOCK Сокет задан неправильно");
-	}break;
-	case(WSAEDESTADDRREQ) : {
-		OutputDebugStringA("WSAEDESTADDRREQ Требуется адрес расположения ");
-	}break;
-	case(WSAEMSGSIZE) : {
-		OutputDebugStringA("WSAEMSGSIZE Сообщение слишком длинное");
-	}break;
-	case(WSAEPROTOTYPE) : {
-		OutputDebugStringA("WSAEPROTOTYPE Неправильный тип протокола для сокета");
-	}break;
-	case(WSAENOPROTOOPT) : {
-		OutputDebugStringA("WSAENOPROTOOPT Ошибка в опции протокола");
-	}break;
-	case(WSAEPROTONOSUPPORT) : {
-		OutputDebugStringA("WSAEPROTONOSUPPORT Протокол не поддерживается ");
-	}break;
-	case(WSAESOCKTNOSUPPORT) : {
-		OutputDebugStringA("WSAESOCKTNOSUPPORT Тип сокета не поддерживается ");
-	}break;
-	case(WSAEOPNOTSUPP) : {
-		OutputDebugStringA("WSAEOPNOTSUPP Операция не поддерживается ");
-	}break;
-	case(WSAEPFNOSUPPORT) : {
-		OutputDebugStringA("WSAEPFNOSUPPORT Тип протоколов не поддерживается");
-	}break;
-	case(WSAEAFNOSUPPORT) : {
-		OutputDebugStringA("WSAEAFNOSUPPORT Тип адресов не поддерживается протоколом");
-	}break;
-	case(WSAEADDRINUSE) : {
-		OutputDebugStringA("WSAEADDRINUSE Адрес уже используется");
-	}break;
-	case(WSAEADDRNOTAVAIL) : {
-		OutputDebugStringA("WSAEADDRNOTAVAIL Запрошенный адрес не может быть использован");
-	}break;
-	case(WSAENETDOWN) : {
-		OutputDebugStringA("WSAENETDOWN Сеть отключена");
-	}break;
-	case(WSAENETUNREACH) : {
-		OutputDebugStringA("WSAENETUNREACH Сеть не достижима ");
-	}break;
-	case(WSAENETRESET) : {
-		OutputDebugStringA("WSAENETRESET Сеть разорвала соединение");
-	}break;
-	case(WSAECONNABORTED) : {
-		OutputDebugStringA("WSAECONNABORTED Программный отказ связи ");
-	}break;
-	case(WSAECONNRESET) : {
-		OutputDebugStringA("WSAECONNRESET Связь восстановлена ");
-	}break;
-	case(WSAENOBUFS) : {
-		OutputDebugStringA("WSAENOBUFS Не хватает памяти для буферов");
-	}break;
-	case(WSAEISCONN) : {
-		OutputDebugStringA("WSAEISCONN Сокет уже подключен");
-	}break;
-	case(WSAENOTCONN) : {
-		OutputDebugStringA("WSAENOTCONN Сокет не подключен");
-	}break;
-	case(WSAESHUTDOWN) : {
-		OutputDebugStringA("WSAESHUTDOWN Нельзя выполнить send: сокет завершил работу");
-	}break;
-	case(WSAETIMEDOUT) : {
-		OutputDebugStringA("WSAETIMEDOUT Закончился отведенный интервал  времени");
-	}break;
-	case(WSAECONNREFUSED) : {
-		OutputDebugStringA("WSAECONNREFUSED Соединение отклоненo");
-	}break;
-	case(WSAEHOSTDOWN) : {
-		OutputDebugStringA("WSAEHOSTDOWN Хост в неработоспособном состоянии");
-	}break;
-	case(WSAEHOSTUNREACH) : {
-		OutputDebugStringA("WSAEHOSTUNREACH Нет маршрута для хоста");
-	}break;
-	case(WSAEPROCLIM) : {
-		OutputDebugStringA("WSAEPROCLIM Слишком много процессов ");
-	}break;
-	case(WSASYSNOTREADY) : {
-		OutputDebugStringA("WSASYSNOTREADY Сеть не доступна ");
-	}break;
-	case(WSAVERNOTSUPPORTED) : {
-		OutputDebugStringA("WSAVERNOTSUPPORTED Данная версия недоступна ");
-	}break;
-	case(WSANOTINITIALISED) : {
-		OutputDebugStringA("WSANOTINITIALISED Не выполнена инициализация WS2_32.DLL");
-	}break;
-	case(WSAEDISCON) : {
-		OutputDebugStringA("WSAEDISCON Выполняется отключение");
-	}break;
-	case(WSATYPE_NOT_FOUND) : {
-		OutputDebugStringA("WSATYPE_NOT_FOUND Класс не найден");
-	}break;
-	case(WSAHOST_NOT_FOUND) : {
-		OutputDebugStringA("WSAHOST_NOT_FOUND Хост не найден ");
-	}break;
-	case(WSATRY_AGAIN) : {
-		OutputDebugStringA("WSATRY_AGAIN Неавторизированный хост не найден ");
-	}break;
-	case(WSANO_RECOVERY) : {
-		OutputDebugStringA("WSANO_RECOVERY Неопределенная  ошибка");
-	}break;
-	case(WSANO_DATA) : {
-		OutputDebugStringA("WSANO_DATA Нет записи запрошенного типа");
-	}break;
-	case(WSA_INVALID_HANDLE) : {
-		OutputDebugStringA("WSA_INVALID_HANDLE Указанный дескриптор события  с ошибкой");
-	}break;
-	case(WSA_INVALID_PARAMETER) : {
-		OutputDebugStringA("WSA_INVALID_PARAMETER Один или более параметров с ошибкой");
-	}break;
-	case(WSA_IO_INCOMPLETE) : {
-		OutputDebugStringA("WSA_IO_INCOMPLETE Объект ввода-вывода не в сигнальном состоянии");
-	}break;
-	case(WSA_IO_PENDING) : {
-		OutputDebugStringA("WSA_IO_PENDING Операция завершится позже");
-	}break;
-	case(WSA_NOT_ENOUGH_MEMORY) : {
-		OutputDebugStringA("WSA_NOT_ENOUGH_MEMORY Не достаточно памяти");
-	}break;
-	case(WSA_OPERATION_ABORTED) : {
-		OutputDebugStringA("WSA_OPERATION_ABORTED Операция отвергнута");
-	}break;
-	case(0) : {
-		OutputDebugStringA("Everything is seems to be OK\n");
-	}break;
+		case (WSAEINTR) : {
+			OutputDebugStringA("WSAEINTR Работа функции прервана");
+		}break;
+		case(WSAEACCES) : {
+			OutputDebugStringA("WSAEACCES Разрешение отвергнуто");
+		}break;
+		case(WSAEFAULT) : {
+			OutputDebugStringA("WSAEFAULT Ошибочный адрес ");
+		}break;
+		case(WSAEINVAL) : {
+			OutputDebugStringA("WSAEINVAL Ошибка в аргументе");
+		}break;
+		case(WSAEMFILE) : {
+			OutputDebugStringA("WSAEMFILE Слишком много файлов открыто");
+		}break;
+		case(WSAEWOULDBLOCK) : {
+			OutputDebugStringA("WSAEWOULDBLOCK Ресурс временно недоступен");
+		}break;
+		case(WSAEINPROGRESS) : {
+			OutputDebugStringA("WSAEINPROGRESS Операция в процессе развития");
+		}break;
+		case(WSAEALREADY) : {
+			OutputDebugStringA("WSAEALREADY Операция уже выполняется");
+		}break;
+		case(WSAENOTSOCK) : {
+			OutputDebugStringA("WSAENOTSOCK Сокет задан неправильно");
+		}break;
+		case(WSAEDESTADDRREQ) : {
+			OutputDebugStringA("WSAEDESTADDRREQ Требуется адрес расположения ");
+		}break;
+		case(WSAEMSGSIZE) : {
+			OutputDebugStringA("WSAEMSGSIZE Сообщение слишком длинное");
+		}break;
+		case(WSAEPROTOTYPE) : {
+			OutputDebugStringA("WSAEPROTOTYPE Неправильный тип протокола для сокета");
+		}break;
+		case(WSAENOPROTOOPT) : {
+			OutputDebugStringA("WSAENOPROTOOPT Ошибка в опции протокола");
+		}break;
+		case(WSAEPROTONOSUPPORT) : {
+			OutputDebugStringA("WSAEPROTONOSUPPORT Протокол не поддерживается ");
+		}break;
+		case(WSAESOCKTNOSUPPORT) : {
+			OutputDebugStringA("WSAESOCKTNOSUPPORT Тип сокета не поддерживается ");
+		}break;
+		case(WSAEOPNOTSUPP) : {
+			OutputDebugStringA("WSAEOPNOTSUPP Операция не поддерживается ");
+		}break;
+		case(WSAEPFNOSUPPORT) : {
+			OutputDebugStringA("WSAEPFNOSUPPORT Тип протоколов не поддерживается");
+		}break;
+		case(WSAEAFNOSUPPORT) : {
+			OutputDebugStringA("WSAEAFNOSUPPORT Тип адресов не поддерживается протоколом");
+		}break;
+		case(WSAEADDRINUSE) : {
+			OutputDebugStringA("WSAEADDRINUSE Адрес уже используется");
+		}break;
+		case(WSAEADDRNOTAVAIL) : {
+			OutputDebugStringA("WSAEADDRNOTAVAIL Запрошенный адрес не может быть использован");
+		}break;
+		case(WSAENETDOWN) : {
+			OutputDebugStringA("WSAENETDOWN Сеть отключена");
+		}break;
+		case(WSAENETUNREACH) : {
+			OutputDebugStringA("WSAENETUNREACH Сеть не достижима ");
+		}break;
+		case(WSAENETRESET) : {
+			OutputDebugStringA("WSAENETRESET Сеть разорвала соединение");
+		}break;
+		case(WSAECONNABORTED) : {
+			OutputDebugStringA("WSAECONNABORTED Программный отказ связи ");
+		}break;
+		case(WSAECONNRESET) : {
+			OutputDebugStringA("WSAECONNRESET Связь восстановлена ");
+		}break;
+		case(WSAENOBUFS) : {
+			OutputDebugStringA("WSAENOBUFS Не хватает памяти для буферов");
+		}break;
+		case(WSAEISCONN) : {
+			OutputDebugStringA("WSAEISCONN Сокет уже подключен");
+		}break;
+		case(WSAENOTCONN) : {
+			OutputDebugStringA("WSAENOTCONN Сокет не подключен");
+		}break;
+		case(WSAESHUTDOWN) : {
+			OutputDebugStringA("WSAESHUTDOWN Нельзя выполнить send: сокет завершил работу");
+		}break;
+		case(WSAETIMEDOUT) : {
+			OutputDebugStringA("WSAETIMEDOUT Закончился отведенный интервал  времени");
+		}break;
+		case(WSAECONNREFUSED) : {
+			OutputDebugStringA("WSAECONNREFUSED Соединение отклоненo");
+		}break;
+		case(WSAEHOSTDOWN) : {
+			OutputDebugStringA("WSAEHOSTDOWN Хост в неработоспособном состоянии");
+		}break;
+		case(WSAEHOSTUNREACH) : {
+			OutputDebugStringA("WSAEHOSTUNREACH Нет маршрута для хоста");
+		}break;
+		case(WSAEPROCLIM) : {
+			OutputDebugStringA("WSAEPROCLIM Слишком много процессов ");
+		}break;
+		case(WSASYSNOTREADY) : {
+			OutputDebugStringA("WSASYSNOTREADY Сеть не доступна ");
+		}break;
+		case(WSAVERNOTSUPPORTED) : {
+			OutputDebugStringA("WSAVERNOTSUPPORTED Данная версия недоступна ");
+		}break;
+		case(WSANOTINITIALISED) : {
+			OutputDebugStringA("WSANOTINITIALISED Не выполнена инициализация WS2_32.DLL");
+		}break;
+		case(WSAEDISCON) : {
+			OutputDebugStringA("WSAEDISCON Выполняется отключение");
+		}break;
+		case(WSATYPE_NOT_FOUND) : {
+			OutputDebugStringA("WSATYPE_NOT_FOUND Класс не найден");
+		}break;
+		case(WSAHOST_NOT_FOUND) : {
+			OutputDebugStringA("WSAHOST_NOT_FOUND Хост не найден ");
+		}break;
+		case(WSATRY_AGAIN) : {
+			OutputDebugStringA("WSATRY_AGAIN Неавторизированный хост не найден ");
+		}break;
+		case(WSANO_RECOVERY) : {
+			OutputDebugStringA("WSANO_RECOVERY Неопределенная  ошибка");
+		}break;
+		case(WSANO_DATA) : {
+			OutputDebugStringA("WSANO_DATA Нет записи запрошенного типа");
+		}break;
+		case(WSA_INVALID_HANDLE) : {
+			OutputDebugStringA("WSA_INVALID_HANDLE Указанный дескриптор события  с ошибкой");
+		}break;
+		case(WSA_INVALID_PARAMETER) : {
+			OutputDebugStringA("WSA_INVALID_PARAMETER Один или более параметров с ошибкой");
+		}break;
+		case(WSA_IO_INCOMPLETE) : {
+			OutputDebugStringA("WSA_IO_INCOMPLETE Объект ввода-вывода не в сигнальном состоянии");
+		}break;
+		case(WSA_IO_PENDING) : {
+			OutputDebugStringA("WSA_IO_PENDING Операция завершится позже");
+		}break;
+		case(WSA_NOT_ENOUGH_MEMORY) : {
+			OutputDebugStringA("WSA_NOT_ENOUGH_MEMORY Не достаточно памяти");
+		}break;
+		case(WSA_OPERATION_ABORTED) : {
+			OutputDebugStringA("WSA_OPERATION_ABORTED Операция отвергнута");
+		}break;
+		case(0) : {
+			OutputDebugStringA("Everything is seems to be OK\n");
+		}break;
 	}
+	OutputDebugStringA("\n");
 }
